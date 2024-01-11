04f11cf42a0a7a38c05e1590c36801b43a8dedd3
Wout Neirynck <wout@monkey-projects.be>
2024-01-11 15:50:41 +0100

    Added publish env vars

--- src/monkey/ci/plugin/clj.clj
+++ src/monkey/ci/plugin/clj.clj
@@ -1,5 +1,8 @@
 (ns monkey.ci.plugin.clj
-  (:require [monkey.ci.build.core :as b]))
+  (:require [clojure.xml :as xml]
+            [monkey.ci.build
+             [api :as api]
+             [core :as b]]))
 
 (def version-regex #"^\d+\.\d+(\.\d+)?$")
 (def all-regex #".*")
@@ -37,6 +40,27 @@
       (clj-deps (str "-X" test-alias))
       (assoc :name "test")))
 
+(defn read-pom-version
+  "Given the step context, reads the `pom.xml` file from the configured location
+   and returns the version tag value."
+  [{:keys [pom-file] :or {pom-file "pom.xml"}} ctx]
+  (->> (xml/parse pom-file)
+       :content
+       (filter (comp (partial = :version) :tag))
+       (first)
+       :content
+       (first)))
+
+(defn- add-version [env
+                    {:keys [version-var pom-version-reader]
+                     :or {version-var "LIB_VERSION"
+                          pom-version-reader read-pom-version}
+                     :as conf}
+                    ctx]
+  (let [v (or (b/tag ctx) (pom-version-reader conf ctx))]
+    (cond-> env
+      v (assoc version-var v))))
+
 (defn deps-publish [{:keys [publish-alias]
                      :or {publish-alias "publish"}
                      :as conf}]
@@ -44,7 +68,10 @@
     (when (should-publish? conf ctx)
       (-> conf
           (clj-deps (str "-X" publish-alias))
-          (assoc :name "deploy")))))
+          (assoc :name "deploy"
+                 :container/env (-> (api/build-params ctx)
+                                    (select-keys ["CLOJARS_USERNAME" "CLOJARS_PASSWORD"])
+                                    (add-version conf ctx)))))))
 
 (defn deps-library
   "Creates a pipeline that tests and deploys a clojure library using deps.edn."
--- test/monkey/ci/plugin/test/clj_test.clj
+++ test/monkey/ci/plugin/test/clj_test.clj
@@ -1,6 +1,8 @@
 (ns monkey.ci.plugin.test.clj-test
   (:require [clojure.test :refer [deftest testing is]]
-            [monkey.ci.plugin.clj :as sut]))
+            [monkey.ci.build.api :as api]
+            [monkey.ci.plugin.clj :as sut])
+  (:import java.io.File))
 
 (def test-step
   (comp first :steps))
@@ -35,20 +37,56 @@
                  :name)))))
 
   (testing "publish step"
-    (testing "`nil` if it's not the main branch or a tag"
-      (is (nil? (-> (sut/deps-library)
-                    (publish-step {:build {:git {:main-branch "main"
-                                                 :ref "refs/heads/other"}}})))))
+    (with-redefs [api/build-params (constantly
+                                    {"CLOJARS_USERNAME" "testuser"
+                                     "CLOJARS_PASSWORD" "testpass"})]
+      (testing "`nil` if it's not the main branch or a tag"
+        (is (nil? (-> (sut/deps-library)
+                      (publish-step {:build {:git {:main-branch "main"
+                                                   :ref "refs/heads/other"}}})))))
 
-    (testing "invokes default container img"
-      (is (= sut/default-deps-img
-             (-> (sut/deps-library)
-                 (publish-step {:build {:git {:main-branch "main"
-                                              :ref "refs/heads/main"}}})
-                 :container/image))))
+      (testing "invokes default container img"
+        (is (= sut/default-deps-img
+               (-> (sut/deps-library)
+                   (publish-step {:build {:git {:main-branch "main"
+                                                :ref "refs/heads/main"}}})
+                   :container/image))))
 
-    (testing "invokes configured container img"
-      (is (= "test-img"
-             (-> (sut/deps-library {:clj-img "test-img"})
-                 (publish-step {:build {:git {:ref "refs/tags/publish-me"}}})
-                 :container/image))))))
+      (testing "invokes configured container img"
+        (is (= "test-img"
+               (-> (sut/deps-library {:clj-img "test-img"})
+                   (publish-step {:build {:git {:ref "refs/tags/publish-me"}}})
+                   :container/image))))
+
+      (testing "takes clojars creds from build params"
+        (is (= ["testuser" "testpass"]
+               (-> (sut/deps-library)
+                   (publish-step {:build {:git {:main-branch "main"
+                                                :ref "refs/heads/main"}}})
+                   :container/env
+                   (select-keys ["CLOJARS_USERNAME" "CLOJARS_PASSWORD"])
+                   (vals)))))
+
+      (testing "adds version env var"
+        (testing "from tag"
+          (is (= "test-version"
+                 (-> (sut/deps-library)
+                     (publish-step {:build {:git {:ref "refs/tags/test-version"}}})
+                     :container/env
+                     (get "LIB_VERSION")))))
+
+        (testing "from `pom.xml`"
+          (is (= "test-version"
+                 (-> (sut/deps-library {:pom-version-reader (constantly "test-version")
+                                        :version-var "MY_VERSION"})
+                     (publish-step {:build {:git {:main-branch "main"
+                                                  :ref "refs/heads/main"}}})
+                     :container/env
+                     (get "MY_VERSION")))))))))
+
+(deftest read-pom-version
+  (testing "reads `pom.xml` file and extracts version"
+    (let [f (File/createTempFile "pom-" ".xml")]
+      (is (nil? (spit f "<project><version>test-version</version></project>")))
+      (is (= "test-version" (sut/read-pom-version {:pom-file (.getCanonicalPath f)} {})))
+      (is (true? (.delete f))))))
