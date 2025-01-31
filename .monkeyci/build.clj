50e9b0c34b5024ffe70eb1aa4360ef7a63d06b7a
Wout Neirynck <wout@monkey-projects.be>
2025-01-31 09:45:15 +0100

    Upgrades
    - build v0.3.1-SNAPSHOT, replaced pom.xml by settings in deps.edn
    - added github plugin

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -1,6 +1,9 @@
-(ns plugin-clj.build
-  (:require [monkey.ci.plugin.clj :as p]))
+(ns build
+  (:require [monkey.ci.plugin
+             [clj :as p]
+             [github :as gh]]))
 
 [(p/deps-library)
  (p/lein-test {:junit-file "junit-lein.xml"
-               :test-job-id "test-lein"})]
+               :test-job-id "test-lein"})
+ (gh/release-job {:dependencies ["publish"]})]
