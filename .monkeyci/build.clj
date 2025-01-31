03496b8ff52b12eff598fd1ea649befd9c03eb55
Wout Neirynck <wout@monkey-projects.be>
2025-01-31 10:06:07 +0100

    Fix lein artifact name

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -4,6 +4,6 @@
              [github :as gh]]))
 
 [(p/deps-library)
- (p/lein-test {:junit-file "junit-lein.xml"
+ (p/lein-test {:junit-file "junit.xml"
                :test-job-id "test-lein"})
  (gh/release-job {:dependencies ["publish"]})]
